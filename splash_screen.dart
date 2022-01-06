e4f9e987b15eab19589a2da40ac5b2aaac59b4f6
jonathan6237 <arnoldjonathan1996@gmail.com>
2022-01-06 19:58:37 +0100

    mise en place de la structure du projet

---
+++ splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:micfi/welcome_screen.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+          duration: 2000,
+          splash: Icon(Icons.money_off_csred_rounded, color: Colors.orangeAccent, size: 100,),
+          nextScreen: WelcomeScreen(),
+          splashTransition: SplashTransition.fadeTransition,
+        );
+  }
+}
