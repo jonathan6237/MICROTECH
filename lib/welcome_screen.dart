d3b4502cee95bd19be74b13b0b31206dd4d41afd
ABANDAZ <97153475+ABANDAZ@users.noreply.github.com>
2022-01-20 21:12:09 +0100

    Create welcome_screen.dart

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
